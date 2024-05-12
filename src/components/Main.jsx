c4862affd96cd8ffb51e71dd191ad07392fef1a7
HectorTT <hectorgt15@gmail.com>
2024-05-12 13:00:54 -0600

    gloria

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,43 @@
-import { useChangeElements } from '../hooks/changeElements'
+import { useChangeElements } from '../hooks/changeElements';
 
-export function Main () {
 
-    const { handleButtonNo, handleButtonYes, textP, image } = useChangeElements()
+
+export function Main() {
+
+    const { handleButtonNo, handleButtonYes, textP, image, textIntro, handleButtonNext } = useChangeElements()
 
     return (
         <>
             <main className='max-w-3xl m-auto w-full h-screen text-center flex justify-center items-center'>
-
                 <div className='flex justify-center items-center flex-col gap-2'>
+                    {/* Título */}
+                    <h1 className='font-sans font-bold text-gray-400 text-2xl'>{textIntro}</h1>
 
-                    <h1 className='font-sans font-bold text-gray-400 text-2xl'> 
-                    Quieres ser mi San Valentin? </h1>
+                    {/* Botón Siguiente (si el texto no es 'Quieres ser mi novia?') */}
+                    {textIntro !== 'Quieres ser mi novia?' && (
+                        <button className='bg-purple-500 w-28 text-white font-bold py-2 px-4 rounded transition-all hover:scale-110' onClick={handleButtonNext}>
+                            &rarr; {/* Flecha a la derecha */}
+                        </button>
+                    )}
 
-                    <img src={image} className='w-72'/>
+                    {/* Imagen */}
+                    <img src={image} className='w-72' alt='Imagen' />
 
-                    <p className='font-mono text-yellow-100'> {textP} </p>
+                    {/* Texto */}
 
-                    <section className='flex gap-3'>
-
-                        <button className='bg-purple-500 w-28 text-white font-bold py-2 px-4 rounded transition-all hover:scale-110' onClick={handleButtonYes}> Si </button>
-
-                        <button className='bg-pink-500 w-20 text-white font-bold py-2 px-4 rounded transition-all hover:scale-110' onClick={handleButtonNo}> No </button>
-                    </section>
-
+                    {/* Botones Si y No */}
+                    {textIntro === 'Quieres ser mi novia?' && (
+                        <><p className='font-mono text-yellow-100'>{textP}</p><section className='flex gap-3'>
+                            <button className='bg-purple-500 w-28 text-white font-bold py-2 px-4 rounded transition-all hover:scale-110' onClick={handleButtonYes}>
+                                Si
+                            </button>
+                            <button className='bg-pink-500 w-20 text-white font-bold py-2 px-4 rounded transition-all hover:scale-110' onClick={handleButtonNo}>
+                                No
+                            </button>
+                        </section></>
+                    )}
                 </div>
-
             </main>
         </>
-    )
+    );
 }
--- src/hooks/changeElements.js
+++ src/hooks/changeElements.js
@@ -3,8 +3,13 @@ import confetti from 'canvas-confetti'
 
 export const useChangeElements = () => {
     const [index, setIndex] = useState(0)
+    const [indexIntro, setIndexIntro] = useState(0)
     const [textP, setTextP] = useState('Vamos, respondeme! ')
-    const [image, setImage] = useState('https://i.pinimg.com/originals/f6/a5/f7/f6a5f7ddff1f05cbcc560256b9f98c2e.gif')
+    //const [image, setImage] = useState('https://i.pinimg.com/originals/f6/a5/f7/f6a5f7ddff1f05cbcc560256b9f98c2e.gif')
+    const [image, setImage] = useState('https://media.tenor.com/8N58zR-v77kAAAAj/love-you.gif')
+
+    const introOptions = [ 'Ya te dije hoy lo hermosa que estas', 'Si no es asi...', '!!!Estas bien hermosa¡¡¡','como estas, como te sientes?', 'ya comiste?','sabes algo...', 'Estas ultimas semanas las he disfrutado mucho', 'gracias a ti', 'me encanta hablar contigo', 'disfruto mucho pasar tiempo contigo', 'Asi que he estado pensando', 'por que si a veces yo pienso', 'pero tampoco pasa muy seguido jajaja', 'pero buen, bueno. YO ...', 'QUIERO PREGUNTARTE ALGO', 'pero no sabia como', 'Y se me ocurrio', 'Hacerte esta pagina', 'Ya que cartulinas no se hacer jeje', 'Quieres ser mi novia?']
+    const [textIntro, setTextIntro] = useState('Hola, Gloria!')
 
     const options = ['Segura?', 'Segurisima?', 'Estas completamente segura?', 'No te arrepentiras?', 'Pero si estas segura?', 'No hay vuelta atras', 'No hay devoluciones', 'No hay garantias', 'No hay reembolsos', 'No hay cambios', 'No hay nada', 'No hay', 'Que no hay']
 
@@ -18,9 +23,19 @@ export const useChangeElements = () => {
 
     const handleButtonYes = () => {
         setImage("https://i.pinimg.com/originals/e4/9d/7b/e49d7b7e965f09e31b498314b02e3662.gif")
-        setTextP('Gracias por aceptar, te amo mucho <3')
+        setTextP('YEY <3')
+        confetti()
+        confetti()
         confetti()
     }
+    const handleButtonNext = () => {
+        setIndexIntro(indexIntro + 1)
+        setTextIntro(introOptions.at(indexIntro))
+        if(indexIntro === introOptions.length - 1){
+            setIndexIntro(introOptions.length - 1)
+            setTextIntro(introOptions.at(indexIntro))
+        }
+    }
 
-    return { handleButtonNo, handleButtonYes, textP, image }
+    return { handleButtonNo, handleButtonYes, textP, image, handleButtonNext, textIntro }
 }
